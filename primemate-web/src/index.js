7bf96dff9491bf0f2f3090632fd15a50d3e09bc5
David Li <david.li@duke.edu>
2021-01-10 01:53:48 -0500

    stuff

---
+++ primemate-web/src/index.js
@@ -0,0 +1,11 @@
+//React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+function App() {
+	return <h1>sup</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
