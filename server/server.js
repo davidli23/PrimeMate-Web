e23428473143ad0f4add43dea50bf0b18e49ba75
David Li <david.li@duke.edu>
2021-01-11 18:08:56 -0500

    asdf

--- server/server.js
+++ server/server.js
@@ -4,10 +4,7 @@ const axios = require('axios');
 const app = express();
 
 app.use((req, res, next) => {
-	res.header(
-		'Access-Control-Allow-Origin',
-		'https://wonderful-perlman-679f67.netlify.app'
-	);
+	res.header('Access-Control-Allow-Origin', '*');
 	res.header(
 		'Access-Control-Allow-Headers',
 		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
