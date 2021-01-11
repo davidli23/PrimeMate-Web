c2b5b88e0eb1d7197efdb641c32ffc12fc8c1edf
David Li <david.li@duke.edu>
2021-01-11 18:13:50 -0500

    cors again

--- server/server.js
+++ server/server.js
@@ -1,23 +1,13 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-	);
-	if (req.method === 'OPTIONS') {
-		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-		return res.status(200).json({});
-	}
-});
+app.use(cors());
 
 app.use('/test', require('./routes/test'));
 app.use('/api/primers/', require('./routes/primers'));
-
-const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
