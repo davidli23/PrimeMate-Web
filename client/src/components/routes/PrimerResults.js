ea3bdf0a352b4a4a08ebb1c74146a2dd34f4c600
David Li <david.li@duke.edu>
2021-01-11 18:17:07 -0500

    finally

--- client/src/components/routes/PrimerResults.js
+++ client/src/components/routes/PrimerResults.js
@@ -5,28 +5,26 @@ import { useParams } from 'react-router-dom';
 const PrimerResults = () => {
 	const [primer, setPrimer] = useState('');
 	const { id } = useParams();
-	// fetch('/api/primers/'.concat(id))
-	// 	.then((res) => res.json())
-	// 	.catch((err) => {
-	// 		console.log('nope');
-	// 	})
-	// 	.then((apiRes) => {
-	// 		setPrimer(apiRes.primerPairs[0].fPrimer);
-	// 	});
-
-	fetch('https://primemate-server.herokuapp.com/test')
+	fetch('/api/primers/'.concat(id))
 		.then((res) => res.json())
-		.then((apiRes) => {
-			setPrimer(apiRes.text);
+		.catch((err) => {
+			console.log('nope');
 		})
-		.catch((err) => console.log(err));
+		.then((apiRes) => {
+			setPrimer(apiRes.primerPairs[0].fPrimer);
+		});
+
+	// fetch('https://primemate-server.herokuapp.com/test')
+	// 	.then((res) => res.json())
+	// 	.then((apiRes) => {
+	// 		setPrimer(apiRes.text);
+	// 	})
+	// 	.catch((err) => console.log(err));
 
 	return (
 		<>
-			<h3>first priemr</h3>
-			<p style={{ wordBreak: 'break-all' }}>
-				{primer} {id}
-			</p>
+			<h3>First Forward Primer</h3>
+			<p style={{ wordBreak: 'break-all' }}>{primer}</p>
 		</>
 	);
 };
--- server/routes/test.js
+++ server/routes/test.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const primers = require('../controllers/primers/primers');
 
 router.get('/', (req, res) => {
-	res.json({ text: 'hello' });
+	res.json({ text: 'This is a test' });
 });
 
 module.exports = router;
