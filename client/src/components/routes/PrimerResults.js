af57b64ca028c19bc424a32358096b40c24d7271
David Li <david.li@duke.edu>
2021-01-11 17:15:56 -0500

    help

--- client/src/components/routes/PrimerResults.js
+++ client/src/components/routes/PrimerResults.js
@@ -5,20 +5,15 @@ import { useParams } from 'react-router-dom';
 const PrimerResults = () => {
 	const [primer, setPrimer] = useState('');
 	const { id } = useParams();
-	// fetch('/api/primers/'.concat(id))
-	// 	.then((res) => res.json())
-	// 	.catch((err) => {
-	// 		console.log('nope');
-	// 	})
-	// 	.then((apiRes) => {
-	// 		setPrimer(apiRes.primerPairs[0].fPrimer);
-	// 	});
-
-	fetch('/')
+	fetch('/api/primers/'.concat(id))
 		.then((res) => res.json())
+		.catch((err) => {
+			console.log('nope');
+		})
 		.then((apiRes) => {
-			setPrimer(apiRes.text);
+			setPrimer(apiRes.primerPairs[0].fPrimer);
 		});
+
 	return (
 		<>
 			<h3>first priemr</h3>
