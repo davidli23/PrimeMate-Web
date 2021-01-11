7b1f15a08f5e0abc9bd4232783337199fcc01aeb
David Li <david.li@duke.edu>
2021-01-11 18:06:11 -0500

    cors

--- server/server.js
+++ server/server.js
@@ -3,6 +3,21 @@ const axios = require('axios');
 
 const app = express();
 
+app.use((req, res, next) => {
+	res.header(
+		'Access-Control-Allow-Origin',
+		'https://wonderful-perlman-679f67.netlify.app'
+	);
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	if (req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+		return res.status(200).json({});
+	}
+});
+
 app.use('/test', require('./routes/test'));
 app.use('/api/primers/', require('./routes/primers'));
 
